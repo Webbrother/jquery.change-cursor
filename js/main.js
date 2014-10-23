e5f58db79bd0bd5d8a9e9e1e8837d1113d2176b3
Vitaliy Komarov <vitaliy_komarov@epam.com>
2014-10-23 14:28:46 +0400

    cursor coordinates changed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#test-id1').changeCursor('img/cursor.png',96,57);
-    $('#test-id2').changeCursor('img/cursor.png',96,57);
-    $('.test-class').changeCursor('img/cursor.png',96,57);
+    $('#test-id1').changeCursor('img/cursor.png',20,0);
+    $('#test-id2').changeCursor('img/cursor.png',32,0);
+    $('.test-class').changeCursor('img/cursor.png',20,0);
 });
